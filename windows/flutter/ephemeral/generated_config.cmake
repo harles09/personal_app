35fa1f1b97ec7f6f8be921843b82c960bb7ebbf7
harles09 <haryobimo8@gmail.com>
2023-03-07 14:00:23 +0700

    initial commit

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\Work\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Bimo Flutter\\personal_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\Work\\flutter"
+  "PROJECT_DIR=D:\\Bimo Flutter\\personal_app"
+  "FLUTTER_ROOT=D:\\Work\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Bimo Flutter\\personal_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Bimo Flutter\\personal_app"
+  "FLUTTER_TARGET=D:\\Bimo Flutter\\personal_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Bimo Flutter\\personal_app\\.dart_tool\\package_config.json"
+)
